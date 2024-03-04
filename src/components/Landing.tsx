28aec72f1c995bf83781d0d260849ed6b5525704
bipin7giri <bipingiri27@gmail.com>
2024-03-05 01:20:01 +0545

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,233 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import CodeEditorWindow from "./CodeEditorWindow";
+import axios from "axios";
+import { classnames } from "../utils/general";
+import { languageOptions } from "../constants/languageOptions";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { defineTheme } from "../lib/defineTheme";
+import useKeyPress from "../hooks/useKeyPress";
+import OutputWindow from "./OutputWindow";
+import CustomInput from "./CustomInput";
+import OutputDetails from "./OutputDetails";
+import ThemeDropdown from "./ThemeDropdown";
+import { Button, Modal } from "antd";
+const javascriptDefault = `/**
+* Problem: Binary Search: Search a sorted array for a target value.
+*/
+
+// Time: O(log n)
+const binarySearch = (arr, target) => {
+ return binarySearchHelper(arr, target, 0, arr.length - 1);
+};
+
+const binarySearchHelper = (arr, target, start, end) => {
+ if (start > end) {
+   return false;
+ }
+ let mid = Math.floor((start + end) / 2);
+ if (arr[mid] === target) {
+   return mid;
+ }
+ if (arr[mid] < target) {
+   return binarySearchHelper(arr, target, mid + 1, end);
+ }
+ if (arr[mid] > target) {
+   return binarySearchHelper(arr, target, start, mid - 1);
+ }
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const target = 5;
+console.log(binarySearch(arr, target));
+`;
+
+const Landing = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    handleCompile();
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [code, setCode] = useState(javascriptDefault);
+  const [customInput, setCustomInput] = useState("");
+  const [outputDetails, setOutputDetails] = useState(null);
+  const [processing, setProcessing] = useState<boolean | null>(null);
+  const [theme, setTheme] = useState<any>("cobalt");
+  const [language, setLanguage] = useState(languageOptions[0]);
+
+  const enterPress = useKeyPress("Enter");
+  const ctrlPress = useKeyPress("Control");
+
+  const onSelectChange = (sl: any) => {
+    console.log("selected Option...", sl);
+    setLanguage(sl);
+  };
+
+  useEffect(() => {
+    if (enterPress && ctrlPress) {
+      console.log("enterPress", enterPress);
+      console.log("ctrlPress", ctrlPress);
+      handleCompile();
+    }
+  }, [ctrlPress, enterPress]);
+  const onChange = (action: any, data: any) => {
+    switch (action) {
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("case not handled!", action, data);
+      }
+    }
+  };
+  const handleCompile = () => {
+    setProcessing(true);
+    const formData = {
+      source_code: JSON.stringify(code),
+    };
+    const options = {
+      method: "POST",
+      url: "http://localhost:8000/api/v1/app",
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+      },
+      data: formData,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log("res.data", response.data);
+        setOutputDetails(response.data);
+        showSuccessToast("Success");
+        setProcessing(false);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        // get error status
+        let status = err.response.status;
+
+        showErrorToast(
+          `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
+          10000
+        );
+        setProcessing(false);
+        console.log("catch block...", error);
+      });
+  };
+
+  function handleThemeChange(th: any) {
+    const theme = th;
+    console.log("theme...", theme);
+
+    if (["light", "vs-dark"].includes(theme.value)) {
+      setTheme(theme);
+    } else {
+      defineTheme(theme.value).then((_) => setTheme(theme));
+    }
+  }
+  useEffect(() => {
+    defineTheme("oceanic-next").then((_) =>
+      setTheme({ value: "oceanic-next", label: "Oceanic Next" })
+    );
+  }, []);
+
+  const showSuccessToast = (msg: any) => {
+    toast.success(msg || `Compiled Successfully!`, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const showErrorToast = (msg?: any, timer?: any) => {
+    toast.error(msg || `Something went wrong! Please try again.`, {
+      position: "top-right",
+      autoClose: timer ? timer : 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <div className="bg-slate-300">
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{
+          className: "bg-red-800",
+        }}
+      >
+        <div className="flex flex-shrink-0 w-[100%] flex-col">
+          <OutputWindow outputDetails={outputDetails} />
+
+          {outputDetails && <OutputDetails outputDetails={outputDetails} />}
+        </div>
+      </Modal>
+
+      <div className="flex flex-row items-center    justify-between w-[95%] mx-auto">
+        {/* <div className="px-4 py-2">
+          <LanguagesDropdown onSelectChange={onSelectChange} />
+        </div> */}
+        <div className="px-4 py-2">
+          <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
+        </div>
+        <div>
+          <Button
+            type="primary"
+            className="bg-blue-600"
+            size="large"
+            shape="default"
+            onClick={showModal}
+          >
+            Run
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-row space-x-4 items-start px-4 py-4">
+        <div className="flex flex-col w-full h-full justify-start items-end">
+          <CodeEditorWindow
+            code={code}
+            onChange={onChange}
+            language={language?.value}
+            theme={theme.value}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Landing;
---
+++ src/components/ThemeDropdown.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Select from "react-select";
+// @ts-ignore
+import monacoThemes from "monaco-themes/themes/themelist";
+import { customStyles } from "../constants/customStyles";
+
+const ThemeDropdown = ({ handleThemeChange, theme }: any) => {
+  return (
+    <Select
+      placeholder={`Select Theme`}
+      // options={languageOptions}
+      options={Object.entries(monacoThemes).map(([themeId, themeName]) => ({
+        label: themeName,
+        value: themeId,
+        key: themeId,
+      }))}
+      value={theme}
+      styles={customStyles}
+      onChange={handleThemeChange}
+    />
+  );
+};
+
+export default ThemeDropdown;
